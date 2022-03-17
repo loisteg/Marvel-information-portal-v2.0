6131ce4fa830f6f1b39f33d4d43c04404a85fef5
Loisteg <loisteg@outlook.com>
2022-03-17 17:02:57 +0200

    Added CSSTranstions on a ComicsList and a CharList

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -44,21 +45,26 @@ const ComicsList = () => {
                 imgStyle = {'objectFit' : 'unset'};
             }
 
+            
             return (
-                <li 
-                className="comics__item"
-                key={item.id}>
-                    <Link to={`/comics/${item.id}`}>
-                        <img src={item.thumbnail} alt={item.title} className={`comics__item-img ${imgStyle}`}/>
-                        <div className="comics__item-name">{item.title}</div>
-                        <div className="comics__item-price">{item.price ? item.price + '$' : "NOT AVAILABLE"}</div>
-                    </Link>
-                </li>
+                <CSSTransition key={item.id} timeout={500} classNames="comics__item">
+                    <li 
+                    className="comics__item"
+                    key={item.id}>
+                        <Link to={`/comics/${item.id}`}>
+                            <img src={item.thumbnail} alt={item.title} className={`comics__item-img ${imgStyle}`}/>
+                            <div className="comics__item-name">{item.title}</div>
+                            <div className="comics__item-price">{item.price ? item.price + '$' : "NOT AVAILABLE"}</div>
+                        </Link>
+                    </li>
+                </CSSTransition>
             )
         });
         return (
             <ul className="comics__grid">
-                {items}
+                <TransitionGroup component={null}>
+                 {items}
+                </TransitionGroup>
             </ul>
         );
     } 

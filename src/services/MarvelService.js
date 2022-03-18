a9e15657298be2b6ea28d3715b47e496f3101f11
Loisteg <loisteg@outlook.com>
2022-03-18 20:43:02 +0200

    Added logic SinglePage + Formik + SearchChar

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -16,6 +16,11 @@ const useMarvelService = () => {
         return _transformCharacter(res.data.results[0]);
     }
 
+    const getCharacterByName = async (name) => {
+        const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
+    }
+
     const getAllComics = async (comicsOffset = 0) => {
         const res = await request(`${_apiBase}comics?limit=8&offset=${comicsOffset}&${_apiKey}`);
         return res.data.results.map(_transformComicsInfo);
@@ -51,7 +56,7 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic}
+    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic, getCharacterByName}
 }
 
 export default useMarvelService;
